bed06216ecf330a92aa29b92b848ec8fc5747960
jandrewreel <ree15008@byui.edu>
2019-01-17 10:50:11 -0700

    page variable

--- web/team/header.php
+++ web/team/header.php
@@ -1,19 +1,20 @@
 <?php
    $page = basename($_SERVER['PHP_SELF']);
+   echo $page;
 
-      if ($page == 'about-us.php') {
-         echo '<a href = "about-us.php"><b>About Us</b></a> -
-         <a href = "home.php">Home</a> -
-         <a href = "login.php">Login</a>'
-      }
-      if (basename(__FILE__) == 'home.php') {
-       echo '<a href = "about-us.php">About Us</a> -
-         <a href = "home.php"><b>Home</b></a> -
-         <a href = "login.php">Login</a>'
-      }
-      if (basename(__FILE__) == 'login.php') {
-         echo '<a href = "about-us.php">About Us</a> -
-         <a href = "home.php">Home</a> -
-         <a href = "login.php"><b>Login</b></a>'
-      }
+      // if ($page == 'about-us.php') {
+      //    echo '<a href = "about-us.php"><b>About Us</b></a> -
+      //    <a href = "home.php">Home</a> -
+      //    <a href = "login.php">Login</a>'
+      // }
+      // if (basename(__FILE__) == 'home.php') {
+      //  echo '<a href = "about-us.php">About Us</a> -
+      //    <a href = "home.php"><b>Home</b></a> -
+      //    <a href = "login.php">Login</a>'
+      // }
+      // if (basename(__FILE__) == 'login.php') {
+      //    echo '<a href = "about-us.php">About Us</a> -
+      //    <a href = "home.php">Home</a> -
+      //    <a href = "login.php"><b>Login</b></a>'
+      // }
 ?>
