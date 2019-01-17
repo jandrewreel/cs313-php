99428fb1a53c4df7605e894f5117259f97682daa
jandrewreel <ree15008@byui.edu>
2019-01-17 10:51:31 -0700

    page variable

--- web/team/header.php
+++ web/team/header.php
@@ -2,11 +2,12 @@
    $page = basename($_SERVER['PHP_SELF']);
    echo $page;
 
-      // if ($page == 'about-us.php') {
-      //    echo '<a href = "about-us.php"><b>About Us</b></a> -
-      //    <a href = "home.php">Home</a> -
-      //    <a href = "login.php">Login</a>'
-      // }
+      if ($page == 'about-us.php') {
+         echo 'HERE';
+         echo '<a href = "about-us.php"><b>About Us</b></a> -
+         <a href = "home.php">Home</a> -
+         <a href = "login.php">Login</a>';
+      }
       // if (basename(__FILE__) == 'home.php') {
       //  echo '<a href = "about-us.php">About Us</a> -
       //    <a href = "home.php"><b>Home</b></a> -
