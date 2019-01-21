d85cfba361ec7e1d3445d2b0c5af726a7b0c10b8
jandrewreel <ree15008@byui.edu>
2019-01-21 14:09:59 -0700

    css

--- web/assignments.php
+++ web/assignments.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <style type="text/css">
+      body {
+         background-image: url("computer.jpg");
+         background-size: cover;
+      }
+   </style>
    <title>Assignments</title>
    <link rel = "stylesheet" type = "text/css" href= "home1.css">
 </head>
