216840e822d021d38ab37de9b4d47ee59317a955
jandrewreel <ree15008@byui.edu>
2019-01-20 21:44:26 -0700

    edits

--- web/homepage.php
+++ web/homepage.php
@@ -4,7 +4,7 @@
    <style type="text/css">
       body {
          background-image: url("mountain1.jpg");
-         background-size: auto;
+         background-size: cover;
       }
    </style>
    <title>Justin's Homepage</title>
