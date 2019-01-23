3bd542894ea3a4f8c2a010e03d883fccbb482e62
jandrewreel <ree15008@byui.edu>
2019-01-23 13:30:27 -0700

    first test

--- web/shopping_cart/header.php
+++ web/shopping_cart/header.php
@@ -5,19 +5,19 @@
    if ($page == 'browse.php') {
       echo '<a href = "browse.php"><b>Browse</b></a> -
             <a href = "cart.php">Shopping Cart</a> -
-            <a href = "checkout.php>Checkout</a>';
+            <a href = "checkout.php">Checkout</a>';
    }
 
    if ($page == 'cart.php') {
       echo '<a href = "browse.php">Browse</a> -
             <a href = "cart.php"><b>Shopping Cart</b></a> -
-            <a href = "checkout.php>Checkout</a>';
+            <a href = "checkout.php">Checkout</a>';
    }
 
    if ($page == 'checkout.php') {
       echo '<a href = "browse.php">Browse</a> -
             <a href = "cart.php">Shopping Cart</a> -
-            <a href = "checkout.php><b>Checkout</b></a>';
+            <a href = "checkout.php"><b>Checkout</b></a>';
    }
 
 ?>
