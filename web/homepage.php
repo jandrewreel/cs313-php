41ef7f42755becd74991581c11c6b1e33e067569
jandrewreel <ree15008@byui.edu>
2019-01-23 12:33:01 -0700

    css

--- web/homepage.php
+++ web/homepage.php
@@ -12,10 +12,11 @@
    <div>
       <h1>About Me</h1>
          <p>
+            <img src="millcreek.jpg">
             I was born and raised in Salt Lake City, UT. Being raised in Utah I grew up to love the
             mountains surronding Salt Lake City. In the summer time I learned all of the trails that
             Utah's multiple canyons housed.  In the winter I was learned how to ski those same trails.
-            <img src="millcreek.jpg">
+
          </p>
 
 
