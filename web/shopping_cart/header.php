605e7afafe07e46f9b40e16f58b73ad207fd0516
jandrewreel <ree15008@byui.edu>
2019-01-23 22:11:59 -0700

    edits

--- web/shopping_cart/header.php
+++ web/shopping_cart/header.php
@@ -6,22 +6,26 @@
 
    if ($page == 'browse.php') {
       echo '<ul>
-               <li><a href = "browse.php"><b>Browse</b></a></li> -
-               <li><a href = "cart.php">Shopping Cart</a><li> -
+               <li><a href = "browse.php"><b>Browse</b></a></li>
+               <li><a href = "cart.php">Shopping Cart</a><li>
                <li><a href = "checkout.php">Checkout</a></li>
             </ul>';
    }
 
    if ($page == 'cart.php') {
-      echo '<a href = "browse.php">Browse</a> -
-            <a href = "cart.php"><b>Shopping Cart</b></a> -
-            <a href = "checkout.php">Checkout</a>';
+      echo '<ul>
+               <li><a href = "browse.php">Browse</a></li>
+               <li><a href = "cart.php"><b>Shopping Cart</b></a></li>
+               <li><a href = "checkout.php">Checkout</a></li>
+            </ul>';
    }
 
    if ($page == 'checkout.php') {
-      echo '<a href = "browse.php">Browse</a> -
-            <a href = "cart.php">Shopping Cart</a> -
-            <a href = "checkout.php"><b>Checkout</b></a>';
+      echo '<ul>
+               <li><a href = "browse.php">Browse</a></li>
+               <li><a href = "cart.php">Shopping Cart</a></li>
+               <li><a href = "checkout.php"><b>Checkout</b></a></li>
+            </ul>';
    }
 
 ?>
