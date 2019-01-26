d9f4cebb3f7a64f2c2cee458f444d56413df114d
jandrewreel <ree15008@byui.edu>
2019-01-25 23:05:27 -0700

    edits

--- web/shopping_cart/browse.php
+++ web/shopping_cart/browse.php
@@ -11,6 +11,15 @@
          <img src="mountain1.jpg">
          <div class="centered"><b>Outdoor Co.</b></div>
       </div>
+      <hr>
+      <h2>Products:</h2>
+      <hr>
+      <div class="product1">
+
+      </div>
+      <div class="product2">
+
+      </div>
    </div>
 </body>
 </html>
