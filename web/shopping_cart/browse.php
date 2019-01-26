813e74d6e5191a5949bfc73c4fb0a779aa97ad16
jandrewreel <ree15008@byui.edu>
2019-01-25 23:06:43 -0700

    edits

--- web/shopping_cart/browse.php
+++ web/shopping_cart/browse.php
@@ -15,10 +15,10 @@
       <h2>Products:</h2>
       <hr>
       <div class="product1">
-
+         <p>Product 1</p>
       </div>
       <div class="product2">
-
+         <p>Product 2</p>
       </div>
    </div>
 </body>
