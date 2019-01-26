570a67e6086ca4d3bae746c585268bbeae0bbc30
jandrewreel <ree15008@byui.edu>
2019-01-25 23:26:47 -0700

    edits

--- web/shopping_cart/browse.php
+++ web/shopping_cart/browse.php
@@ -17,12 +17,15 @@
    <div class="row">
       <div class="column">
          <h2>Product 1:</h2>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
       </div>
       <div class="column">
          <h2>Product 2:</h2>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
       </div>
       <div class="column">
          <h2>Product 3:</h2>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
       </div>
    </div>
       <!-- <div class="row">
