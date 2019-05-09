ee9ff553b385b5210f30fe92a4fdb2923718809d
jandrewreel <ree15008@byui.edu>
2019-05-09 16:37:39 -0600

    edits

--- web/shopping_cart/browse.php
+++ web/shopping_cart/browse.php
@@ -1,10 +1,10 @@
 <?php
    session_start();
 
-   if (empty($_SESSION['cart'])) {
-      $_SESSION['cart'] = array();
-   }
-
+   // if (empty($_SESSION['cart'])) {
+   //    $_SESSION['cart'] = array();
+   // }
+   $_SESSION['cart'] = array();
 
 ?>
 
