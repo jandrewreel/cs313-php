2398f80e262eab97eafd24152184659fffcc45f1
jandrewreel <ree15008@byui.edu>
2019-06-09 00:00:21 -0600

    final updates

--- web/project01/viewWorkouts.php
+++ web/project01/viewWorkouts.php
@@ -146,10 +146,23 @@
       <option value="8">8</option>
    </select>
 
+   Reps:
+   <select name="reps">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+   </select>
 
 
-   <!-- Sets:<input type="text" name="sets"><br> -->
-   Reps:<input type="text" name="reps"><br>
+
+
+   <!-- Sets:<input type="text" name="sets"><br>
+   Reps:<input type="text" name="reps"><br> -->
 
    <input type="submit" name="submit">
    </div>
