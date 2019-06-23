bd04756ef7247b191954a1d3dbb2964cfc529bf0
jandrewreel <ree15008@byui.edu>
2019-06-22 18:13:58 -0600

    package update

--- web/prove09/server.js
+++ web/prove09/server.js
@@ -19,38 +19,39 @@ app.listen(port, function() {
 });
 
 function handleMath(request, response) {
-   const operation = request.query.operation;
-   const operand1 = Number(request.query.operand1);
-   const operand2 = Number(request.query.operand2);
+   const mailType = request.query.mailType;
+   const weight = Number(request.query.weight);
+
 
    // TODO: Here we should check to make sure we have all the correct parameters
 
-   computeOperation(response, operation, operand1, operand2);
+   computeOperation(response, mailType, weight);
 }
 
-function computeOperation(response, op, left, right) {
-   //op = op.toLowerCase();
+function computeOperation(response, mailType, weight) {
 
    let result = 0;
 
-   if (op == "add") {
-      result = left + right;
-   } else if (op == "subtract") {
-      result = left - right;
-   } else if (op == "multiply") {
-      result = left * right;
-   } else if (op == "divide") {
-      result = left / right;
-   } else {
-      // It would be best here to redirect to an "unknown operation"
-      // error page or something similar.
+   if (mailType == "Letters (Stamped)") {
+      result = "Stamped Letters";
    }
 
+   // else if (op == "subtract") {
+   //    result = left - right;
+   // } else if (op == "multiply") {
+   //    result = left * right;
+   // } else if (op == "divide") {
+   //    result = left / right;
+   // } else {
+   //    // It would be best here to redirect to an "unknown operation"
+   //    // error page or something similar.
+   // }
+
    // Set up a JSON object of the values we want to pass along to the EJS result page
-   const params = {operation: op, left: left, right: right, result: result};
+   //const params = {operation: op, left: left, right: right, result: result};
 
    // Render the response, using the EJS page "result.ejs" in the pages directory
    // Makes sure to pass it the parameters we need.
-   response.render('views/results', params);
+   response.render('views/results', result);
 
 }
